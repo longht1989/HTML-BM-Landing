4bc7040fc9af9bbf844f00420b595e6f06c6d435
longht1989 <thanhlong.vn09@gmail.com>
2018-02-12 10:14:58 +0700

    Publish bm landing

---
+++ build/Jscripts/custom.js
@@ -0,0 +1,9 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+$(function() {
+    // aminate fade in index section
+    window.sr = ScrollReveal({ reset: true });
+    sr.reveal('.foo');
+});
